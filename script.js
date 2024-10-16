d01c35644fd1095f6a81db386d39ae2b7b83b8bc
F1nndegamer <finnnijpels2@gmail.com>
2024-10-16 20:07:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         const selectElement = document.getElementById('guests');
+
+        // Function to update the color based on selected option
+        function updateSelectColor() {
+            if (selectElement.value === "") {
+                selectElement.style.color = '#9e9e9e'; // Placeholder color
+            } else {
+                selectElement.style.color = 'black'; // Black color for other options
+            }
+        }
+
         // Set initial color
         updateSelectColor();
 
