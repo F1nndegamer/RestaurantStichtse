e90898a20bc3158dd85074b20f3a22e4f9099817
F1nndegamer <finnnijpels2@gmail.com>
2024-10-16 16:26:09 +0200

    chnaged

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('dinner-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Bedankt voor uw aanmelding! Wij nemen spoedig contact met u op.');
-    this.submit();
-});
